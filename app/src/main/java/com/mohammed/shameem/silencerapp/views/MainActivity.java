5d232227d78e4c98350cd8a09ddcad47b0b9237c
mohammedshameem <shameem@xminds.in>
2016-09-08 10:38:16 +0530

    Changes

--- app/src/main/java/com/mohammed/shameem/silencerapp/views/MainActivity.java
+++ app/src/main/java/com/mohammed/shameem/silencerapp/views/MainActivity.java
@@ -16,13 +16,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        PackageManager p = getPackageManager();
-        ComponentName componentName = new ComponentName(this, com.mohammed.shameem.silencerapp.views.MainActivity.class);
-        p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
         setContentView(R.layout.activity_main);
-        p = getPackageManager();
-        componentName = new ComponentName(this, com.mohammed.shameem.silencerapp.views.MainActivity.class); // activity which is first time open in manifiest file which is declare as <category android:name="android.intent.category.LAUNCHER" />
-        p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
         finish();
         Log.e("MainActivity", "OnCreate");
         MainActivity.this.startService(new Intent(this, WifiFinderService.class));
